92d260028785d5a2bb990e301aa09436ec394c82
Adrian Zygmunt <adrianzygmunt2008@gmail.com>
2026-03-29 15:09:34 +0200

    Python-day1

---
+++ Python--Learn/zmienne.py
@@ -0,0 +1,21 @@
+#zmienne
+x = 5
+y = 10
+print(x + y)
+imie = "Jan"
+print("Cześć, " + imie + "!")
+#zmienne mogą być różnego typu
+wiek = 25
+print("Mam " + str(wiek) + " lat.")
+#zmienne mogą być nadpisywane
+wiek = 30
+print("Mam " + str(wiek) + " lat.")
+float_var = 3.14
+print("Liczba Pi to " + str(float_var))
+#zmienne mogą być używane do przechowywania wyników obliczeń
+a = 10
+b = 20
+c = a + b
+print("Wynik dodawania to " + str(c))
+char_var = 'A'
+print("Pierwsza litera alfabetu to " + char_var)
