8b86c3d7862ad6f95926aafbfcd816ba302a9fda
Adrian Zygmunt <adrianzygmunt2008@gmail.com>
2026-03-31 01:07:21 +0200

    liczby

---
+++ Python--Learn/pracowanie_z_liczbami.py
@@ -0,0 +1,62 @@
+#integer - liczby całkowite
+num1 = 5
+num2 = 10
+# Dodawanie
+suma = num1 + num2
+# Odejmowanie
+roznica = num1 - num2
+# Mnożenie
+iloczyn = num1 * num2
+# Dzielenie
+iloraz = num1 / num2
+print("Suma:", suma)# 15
+print("Różnica:", roznica)# -5
+print("Iloczyn:", iloczyn)# 50
+print("Iloraz:", iloraz)# 0.5
+#float - liczby zmiennoprzecinkowe
+num3 = 3.14
+num4 = 2.71
+# Dodawanie
+suma_float = num3 + num4
+# Odejmowanie
+roznica_float = num3 - num4
+# Mnożenie
+iloczyn_float = num3 * num4
+# Dzielenie
+iloraz_float = num3 / num4
+print("Suma float:", suma_float)# 5.85
+print("Różnica float:", roznica_float)# 0.43
+print("Iloczyn float:", iloczyn_float)# 8.5094
+print("Iloraz float:", iloraz_float)# 1.158678
+
+my_float = 3.14
+my_int = 34
+suma = my_float + my_int
+print("suma:", suma) # 37.14
+print(type(suma)) # <class 'float'>
+
+#modulo - reszta z dzielenia
+num5 = 10
+num6 = 3
+reszta = num5 % num6
+print("Reszta z dzielenia:", reszta) # 1
+#// - dzielenie całkowite
+dzielenie_calkowite = num5 // num6
+print("Dzielenie całkowite:", dzielenie_calkowite) # 3
+#potęgowanie
+potega = num5 ** num6
+print("Potęgowanie:", potega) # 1000
+#float() - konwersja na float
+num7 = 5
+num7_float = float(num7)
+print("num7 jako float:", num7_float) # 5.0
+print(type(num7_float)) # <class 'float'>
+#int() - konwersja na int
+num8 = 3.14
+num8_int = int(num8)
+print("num8 jako int:", num8_int) # 3
+print(type(num8_int)) # <class 'int'>
+my_string = "123"
+my_string_int = int(my_string)
+print("my_string jako int:", my_string_int) # 123
+print(type(my_string_int)) # <class 'int'>
