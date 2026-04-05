a9dedd757b68fd51ec20be207af6b83a52994e50
Adrian Zygmunt <adrianzygmunt2008@gmail.com>
2026-04-05 23:44:05 +0200

    Python

---
+++ Python--Learn/funkcje.py
@@ -0,0 +1,6 @@
+#wbudowane funkcje
+print("Hello World") #wypisuje tekst na ekranie
+len("Hello") #zwraca długość tekstu, w tym przypadku 5
+type(3.14) #zwraca typ danych, w tym przypadku <class '
+input("Podaj swoje imię: ") #pobiera dane od użytkownika i zwraca je jako string
+int("123") #konwertuje string na liczbę całkowitą, w
