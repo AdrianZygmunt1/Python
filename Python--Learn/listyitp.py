0980317e80c88a784beae4bd1141911fda0c63a9
Adrian Zygmunt <adrianzygmunt2008@gmail.com>
2026-04-09 22:34:07 +0200

    Listy

---
+++ Python--Learn/listyitp.py
@@ -0,0 +1,18 @@
+cities = ['London','Tokyo','Los Angeles']
+print(cities[0])
+print(cities[-1])
+print(len(cities))#Wynik 3
+cities[0]='Warsaw'
+del cities[2]
+print('Warsaw' in cities)
+print(cities)
+name = "Jessica"
+nameList = list(name)
+print(nameList)
+Person = ['Max',20,['happy','nervous','sad']]
+print(Person[2][1])
+nameR ,age ,mood = Person
+print(nameR)
+print(age)
+print(mood)
+print()
