6a00fcafa031f88d3de14d13863e4c3219604f84
Adrian Zygmunt <adrianzygmunt2008@gmail.com>
2026-04-10 20:42:12 +0200

    Krotka

--- Python--Learn/listyitp.py
+++ Python--Learn/listyitp.py
@@ -39,6 +39,36 @@ print(numbers)
 numbers = [2, 1, 3, 5, 4, 2.5]
 numbers.sort()#sortuje elementy na miesjcu czyli w tej samej liscie
 print(numbers)
+numbers.reverse()#odwraca liste
+print(numbers)
 numbers = [2, 1, 3, 5, 4, 2.5]
 sorted_numbers = sorted(numbers)# zwraca nową posortowaną listę zamiast modyfikować oryginalną listę
 print(sorted_numbers)
+Color = ['Blue','Red','Black','Brown']
+print(Color.index('Black'))#łuży to do znalezienia pierwszego indeksu, w którym można znaleźć element na liście
+developer = ('Alice', 34, 'Rust Developer')#Krotka zawiera mieszany zestwa typow
+developer[1] # 34
+numbers[-2] # 34
+developer = 'Jessica'
+tuple(developer) # ('J', 'e', 's', 's', 'i', 'c', 'a')
+languages = ('Rust', 'Java', 'Python', 'C++', 'Rust', 'Python')
+'C++' in languages #True
+developer = ('Alice', 34, 'Rust Developer')
+name, age, job = developer
+
+print(name) # 'Alice'
+print(age) # 34
+print(job) # 'Rust Developer'
+name, *rest = developer
+
+print(name) # 'Alice'
+print(rest) # [34, 'Rust Developer']
+print(languages[0:4])#wyznacza cala krotke
+print(languages.count('C++'))#liczy ile jest takiego stringu
+print(languages.index('Java'))#1
+print(languages.index('Python', 3))#tu kazemy gdzie rozpocząć wyszukiwanie ciągu Python
+print(languages.index('Python', 2, 5))
+numbers = (13, 2, 78, 3, 45, 67, 18, 7)
+print(sorted(numbers))# [2, 3, 7, 13, 18, 45, 67, 78]
+print(sorted(languages, key=len))#przykład użycia key argument do sortowania elementów w krotce według długości
+#oglonie krotka jest stala nie mozna nia operowac jak lista jedynie odczytywac
